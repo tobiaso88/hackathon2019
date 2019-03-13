0a7bf6c80852ac87684bb108263d979cf3791f6c
Tobias Olsson <info@tobias-olsson.se>
2019-03-13 15:05:33 +0100

    New build

--- public/js/app.js
+++ public/js/app.js
@@ -49148,8 +49148,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/fredrikolsson/Sites/hackathon2019/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/fredrikolsson/Sites/hackathon2019/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/tobias/Sites/other/hackathon/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/tobias/Sites/other/hackathon/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
