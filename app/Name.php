8a611bf9239e617842706c5169f4cb3a7b805c2a
Tobias Olsson <info@tobias-olsson.se>
2019-03-13 11:10:32 +0100

    Add state relation

--- app/Name.php
+++ app/Name.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Name extends Model
 {
-    //
+    public function stateRelation()
+    {
+        return $this->hasOne(State::class, 'abbreviation', 'state');
+    }
 }
