ad16dbd00be2c97f547eb0b925b0e8dac9d17dc4
Tobias Olsson <info@tobias-olsson.se>
2019-03-13 10:25:29 +0100

    Add name table seeder

---
+++ database/seeds/NamesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NamesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($year = 1880; $year <= 2017; $year++) {
+            $file = file(Storage::disk('seeding')->path('names') . '/yob' . $year . '.TXT');
+            foreach ($file as $lineNumber => $line) {
+                if (empty($line)) {
+                    continue;
+                }
+                $col = explode(",", $line);
+                list($name, $gender, $amount) = $col;
+
+                DB::table('names')->insert([
+                    'name' => $name,
+                    'gender' => $gender,
+                    'year' => $year,
+                    'amount' => $amount,
+                ]);
+            }
+        }
+    }
+}
